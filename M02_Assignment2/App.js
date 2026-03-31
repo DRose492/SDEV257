81b86ce580edd7f3a9d39de151f6e15ea0b3bfee
DRose492 <146631485+DRose492@users.noreply.github.com>
2026-03-30 20:48:19 -0500

    Add files via upload

---
+++ M02_Assignment2/App.js
@@ -0,0 +1,38 @@
+import { Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import PlanetsScreen from './screens/PlanetsScreen';
+import FilmsScreen from './screens/FilmsScreen';
+import SpaceshipsScreen from './screens/SpaceshipsScreen';
+
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+function IOSNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Planets" component={PlanetsScreen} />
+      <Tab.Screen name="Films" component={FilmsScreen} />
+      <Tab.Screen name="Spaceships" component={SpaceshipsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function AndroidNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Planets" component={PlanetsScreen} />
+      <Drawer.Screen name="Films" component={FilmsScreen} />
+      <Drawer.Screen name="Spaceships" component={SpaceshipsScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {Platform.OS === 'ios' ? <IOSNavigator /> : <AndroidNavigator />}
+    </NavigationContainer>
+  );
+}
