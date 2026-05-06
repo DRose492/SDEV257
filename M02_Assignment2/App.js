fb8ac5f8e7d65a87291d27d441ebd7e1e6001d7c
DRose492 <146631485+DRose492@users.noreply.github.com>
2026-05-05 20:18:20 -0500

    Update App.js

--- M02_Assignment2/App.js
+++ M02_Assignment2/App.js
@@ -1,41 +1,84 @@
-import { Platform } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Platform } from 'react-native';
+
 import PlanetsScreen from './screens/Planets_Screen';
 import FilmsScreen from './screens/Films_Screen';
 import SpaceshipsScreen from './screens/Spaceships_Screen';
-import NetworkGuard from './NetworkGuard';
+
+import PlanetDetailScreen from './screens/Planet_Detail_Screen';
+import FilmDetailScreen from './screens/Film_Detail_Screen';
+import StarshipDetailScreen from './screens/Starship_Detail_Screen';
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
 
-function IOSNavigator() {
+const headerStyle = {
+  headerStyle: {
+    backgroundColor: '#0d0d0d',
+  },
+  headerTitleStyle: {
+    color: '#FFE81F',
+    fontWeight: 'bold',
+  },
+  headerTintColor: '#FFE81F',
+};
+
+function PlanetsStackNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Planets" component={PlanetsScreen} />
-      <Tab.Screen name="Films" component={FilmsScreen} />
-      <Tab.Screen name="Spaceships" component={SpaceshipsScreen} />
+    <Stack.Navigator screenOptions={headerStyle}>
+      <Stack.Screen name="Planets" component={PlanetsScreen} />
+      <Stack.Screen name="PlanetDetail" component={PlanetDetailScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function FilmsStackNavigator() {
+  return (
+    <Stack.Navigator screenOptions={headerStyle}>
+      <Stack.Screen name="Films" component={FilmsScreen} />
+      <Stack.Screen name="FilmDetail" component={FilmDetailScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function SpaceshipsStackNavigator() {
+  return (
+    <Stack.Navigator screenOptions={headerStyle}>
+      <Stack.Screen name="Spaceships" component={SpaceshipsScreen} />
+      <Stack.Screen name="StarshipDetail" component={StarshipDetailScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function Tabs() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="PlanetsTab" component={PlanetsStackNavigator} options={{ title: "Planets" }} />
+      <Tab.Screen name="FilmsTab" component={FilmsStackNavigator} options={{ title: "Films" }} />
+      <Tab.Screen name="SpaceshipsTab" component={SpaceshipsStackNavigator} options={{ title: "Spaceships" }} />
     </Tab.Navigator>
   );
 }
 
-function AndroidNavigator() {
+function DrawerNav() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Planets" component={PlanetsScreen} />
-      <Drawer.Screen name="Films" component={FilmsScreen} />
-      <Drawer.Screen name="Spaceships" component={SpaceshipsScreen} />
+    <Drawer.Navigator screenOptions={{ headerShown: false }}>
+      <Drawer.Screen name="PlanetsDrawer" component={PlanetsStackNavigator} options={{ title: "Planets" }} />
+      <Drawer.Screen name="FilmsDrawer" component={FilmsStackNavigator} options={{ title: "Films" }} />
+      <Drawer.Screen name="SpaceshipsDrawer" component={SpaceshipsStackNavigator} options={{ title: "Spaceships" }} />
     </Drawer.Navigator>
   );
 }
 
 export default function App() {
   return (
-    <NetworkGuard>
-      <NavigationContainer>
-        {Platform.OS === 'ios' ? <IOSNavigator /> : <AndroidNavigator />}
-      </NavigationContainer>
-    </NetworkGuard>
+    <NavigationContainer>
+      {Platform.OS === 'ios' ? <Tabs /> : <DrawerNav />}
+    </NavigationContainer>
   );
 }
